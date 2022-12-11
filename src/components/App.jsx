73bfddcf09305630fb61554382ece0a869e78ecd
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-12-11 19:54:12 +0200

    add code from 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
+import Profile from "./Profile/Profile";
+import user from "./Profile/user.json";
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -10,7 +13,16 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+      
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
     </div>
   );
 };
+
+
