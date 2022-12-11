859d8d7d33690f916d75086cf90e8e93b44f47da
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-12-11 22:36:46 +0200

    add code from 2 task

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import style from '../Statistics/Statistics.module.css';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={style.statistics}>
+      {title && <h2 className={style.title}>{title}</h2>}
+
+      <ul className={style.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={style.item}
+              key={id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={style.label}>{label}</span>
+              <span className={style.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
